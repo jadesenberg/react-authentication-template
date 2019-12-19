6bb0a2e517f2d95a14918147dbc79b222bb60a04
Jade Bautista <edaj.bautista@gmail.com>
2019-12-19 22:44:08 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, Redirect, RouteComponentProps } from "react-router-dom";
+
+import { useUserState } from "./contexts/UserContext";
+import "./App.css";
+import Login from "./pages/login";
+
+import Layout from "./pages/layout";
+
+interface IProps {
+	component?:
+		| React.ComponentType<RouteComponentProps<any>>
+		| React.ComponentType<any>;
+	exact?: boolean;
+	path?: string | string[];
+	render?: (props: RouteComponentProps<any>) => React.ReactNode;
+}
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	return (
+		<Switch>
+			<Route
+				exact
+				path="/"
+				render={() => <Redirect to="/app/dashboard" />}
+			/>
+			<Route
+				exact
+				path="/app"
+				render={() => <Redirect to="/app/dashboard" />}
+			/>
+			<PublicRoute path="/login" component={Login} />
+			<PrivateRoute path="/app/" component={Layout} />
+		</Switch>
+	);
+};
+
+const PrivateRoute = ({ component: Component, ...rest }: IProps) => {
+	const { userState } = useUserState();
+	if (!Component) return null;
+	console.log(userState.user);
+	return (
+		<Route
+			{...rest}
+			render={props =>
+				userState.isAuthenticated ? (
+					<Component {...props} />
+				) : (
+					<Redirect
+						to={{
+							pathname: "/login"
+						}}
+					/>
+				)
+			}
+		/>
+	);
+};
+
+const PublicRoute = ({ component: Component, ...rest }: IProps) => {
+	const { userState } = useUserState();
+
+	if (!Component) return null;
+	return (
+		<Route
+			{...rest}
+			render={props =>
+				userState.isAuthenticated ? (
+					<Redirect
+						to={{
+							pathname: "/"
+						}}
+					/>
+				) : (
+					<Component {...props} />
+				)
+			}
+		/>
+	);
+};
 
 export default App;
